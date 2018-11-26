2794d575aee2912af1be26001890452969073b07
ders99 <43677403+ders99@users.noreply.github.com>
2018-11-26 10:47:02 -0600

    Updated Main
    Removed unnecessary commented code, added buttons for users to add and remove foods and load/save to file, along with filters for the food list.

--- Main.java
+++ Main.java
@@ -8,61 +8,57 @@ import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.Border;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	// @Override
-	// public void start(Stage primaryStage) {
-	// try {
-	// BorderPane root = new BorderPane();
-	// Scene scene = new Scene(root, 600, 400); // width, height
-	// scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-	// primaryStage.setScene(scene);
-	// primaryStage.show();
-	// } catch (Exception e) {
-	// e.printStackTrace();
-	// }
-	// }
-
-	TextArea logging;
 
 	@Override
 	public void start(Stage primaryStage) {
 		primaryStage.setTitle("Food Query and Meal Analysis");
-		logging = new TextArea();
-		logging.setMaxWidth(350);
-		logging.setMaxHeight(350);
-		Label foodLbl = new Label(" All Foods");
-
-		// Button btn = new Button();
-		// btn.setText("Say 'Hello World'");
-		// btn.setOnAction(new EventHandler<ActionEvent>() {
-		//
-		// @Override
-		// public void handle(ActionEvent event) {
-		// System.out.println("Hello World!");
-		// }
-		// });
+		Text foodLbl = new Text("All Foods");
+		foodLbl.setFont(Font.font("Verdana", FontWeight.BOLD, 15));
 		ObservableList<String> foodNames = FXCollections.observableArrayList("Apples", "Asparagus", "Bacon", "Bananas",
 				"Celery", "Cranberries", "Eggs", "Gravy", "Green beans");
-		ListView<String> listView = new ListView<String>(foodNames);
-		listView.setPrefSize(150, 120);
+		ListView<String> listOfFoods = new ListView<String>(foodNames);
+		listOfFoods.setPrefSize(150, 120);
+		HBox filterHbox = new HBox();
+		filterHbox.setSpacing(10);
+		Label filterLbl = new Label("Filter: ");
+		CheckBox nameFilter = new CheckBox("Name");
+		nameFilter.setIndeterminate(false);
+		CheckBox nutrientFilter = new CheckBox("Nutrients");
+		nutrientFilter.setIndeterminate(false);
+		filterHbox.getChildren().addAll(filterLbl, nameFilter, nutrientFilter);
 		VBox vbox = new VBox();
-		vbox.setSpacing(10);
-		vbox.getChildren().addAll(foodLbl, listView);
+		vbox.setMaxWidth(200);
+		vbox.setSpacing(12);
+		Button addToMealBtn = new Button("Add to Meal");
+		addToMealBtn.setPrefSize(200, 20);
+		Button addNewFoodBtn = new Button(" + Add New Food");
+		addNewFoodBtn.setPrefSize(200, 20);
+		Button removeFoodBtn = new Button(" - Remove Food");
+		removeFoodBtn.setPrefSize(200, 20);
+		Button loadFromFileBtn = new Button("Load from File");
+		loadFromFileBtn.setPrefSize(200, 20);
+		Button saveToFileBtn = new Button("Save to File");
+		saveToFileBtn.setPrefSize(200, 20);
+		vbox.getChildren().addAll(foodLbl, filterHbox, listOfFoods, addToMealBtn, addNewFoodBtn, removeFoodBtn,
+				loadFromFileBtn, saveToFileBtn);
 		HBox hbox = new HBox();
 		hbox.getChildren().addAll(vbox);
 		primaryStage.setScene(new Scene(hbox, 800, 400));
-		// StackPane root = new StackPane();
-		// root.getChildren().add(btn);
-		// primaryStage.setScene(new Scene(root, 600, 400));
 		primaryStage.show();
 	}
 
