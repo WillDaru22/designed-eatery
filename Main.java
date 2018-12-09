48348bd21f0cfc80ff418613a7003080063ec069
ders99 <43677403+ders99@users.noreply.github.com>
2018-12-09 01:26:59 -0600

    Changed a lot of stuff
    Changed the layout/font of the GUI to make it readable across different operating systems. Implemented the load from file and save to file buttons.

--- Main.java
+++ Main.java
@@ -17,6 +17,9 @@
 
 package application;
 
+import java.io.File;
+import java.util.ArrayList;
+
 /* imported JavaFX add-ons*/
 import javafx.application.Application;
 import javafx.collections.FXCollections;
@@ -26,7 +29,6 @@ import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
@@ -36,6 +38,7 @@ import javafx.scene.shape.Line;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 /**
@@ -45,37 +48,48 @@ import javafx.stage.Stage;
  *
  */
 public class Main extends Application {
+	private ArrayList<FoodItem> foodItemList;
+	ArrayList<String> foodNameList;
+	ObservableList<String> foodNames;
+	ListView<String> listOfFoods;
 
 	@Override
 	public void start(Stage primaryStage) {
+		FoodData foodData = new FoodData();
 
 		// display title
 		primaryStage.setTitle("Food Query and Meal Analysis");
 
 		// display vertical box at top left
 		VBox vbox1 = new VBox();
-		vbox1.setMaxWidth(200);
-		vbox1.setMinWidth(200);
+		vbox1.setMaxWidth(300);
+		vbox1.setMinWidth(300);
 		vbox1.setSpacing(12);
 
 		// text in vertical box at topmost
-		Text foodLbl = new Text("        All Foods");
-		foodLbl.setFont(Font.font("Verdana", FontWeight.EXTRA_BOLD, 20));
+		Text foodLbl = new Text("            All Foods");
+		foodLbl.setFont(Font.font("Abel", FontWeight.EXTRA_BOLD, 28));
 
-		// food list in vertical box. This is hard coded at the moment TODO: import from
-		// file
-		ObservableList<String> foodNames = FXCollections.observableArrayList("Apples", "Asparagus", "Avacado", "Bacon",
-				"Bananas", "Bok Choy", "Celery", "Chocolate", "Cranberries", "Eggs", "Fries", "Gravy", "Green beans",
-				"Ham", "Ice cream", "Jelly", "Lemons", "Lobster", "Mashed potatoes", "Melons", "Noodles", "Pineapples",
-				"Ribs", "Steak", "Turkey", "Yogurt");
-		ListView<String> listOfFoods = new ListView<String>(foodNames);
-		listOfFoods.setPrefSize(150, 120);
+		// food list in vertical box.
+		foodItemList = (ArrayList<FoodItem>) foodData.getAllFoodItems();
+		foodNameList = new ArrayList<String>();
+		for (int i = 0; i < foodItemList.size(); i++)
+			foodNameList.add(foodItemList.get(i).getName());
+		foodNames = FXCollections.observableArrayList(foodNameList);
+		listOfFoods = new ListView<String>(foodNames);
+		listOfFoods.setMaxWidth(300);
+		listOfFoods.setMinWidth(300);
+		listOfFoods.setMaxHeight(200);
+		listOfFoods.setMinHeight(200);
 
 		// horizontal box that filters by names and nutrients
 		HBox filterHbox = new HBox();
 		filterHbox.setSpacing(10);
-		Label filterLbl = new Label("Filter: ");
+		Label filterLbl = new Label("   Filter by: ");
+		filterLbl.setFont(Font.font("Abel", FontWeight.NORMAL, 20));
 		Button nameFilter = new Button("Name");
+		nameFilter.setFont(Font.font("Abel", FontWeight.NORMAL, 15));
+		nameFilter.setMinWidth(75);
 		nameFilter.setOnAction(new EventHandler<ActionEvent>() {
 
 			/**
@@ -89,6 +103,7 @@ public class Main extends Application {
 					nameBox.setSpacing(10);
 					TextField nameFilterField = new TextField();
 					Button confirmName = new Button("Filter");
+					confirmName.setFont(Font.font("Abel", FontWeight.NORMAL, 15));
 					confirmName.setPrefSize(100, 20);
 					nameBox.getChildren().addAll(nameFilterField, confirmName);
 					Scene scene = new Scene(nameBox, 260, 50);
@@ -103,11 +118,10 @@ public class Main extends Application {
 			}
 		});
 
-		// CheckBox nameFilter = new CheckBox("Name");
-		// nameFilter.setIndeterminate(false);
-
 		// nutrient button that filters by nutritional contents
 		Button nutrientFilter = new Button("Nutrients");
+		nutrientFilter.setFont(Font.font("Abel", FontWeight.NORMAL, 15));
+		nutrientFilter.setMinWidth(75);
 		nutrientFilter.setOnAction(new EventHandler<ActionEvent>() {
 
 			// event handle method that enables user to filter through nutrients
@@ -119,20 +133,24 @@ public class Main extends Application {
 					nutrientBox.setSpacing(10);
 					TextField nutrientName = new TextField();
 					nutrientName.setPromptText("Nutrient");
-					nutrientName.setMaxSize(100, 27);
-					nutrientName.setMinSize(100, 27);
+					nutrientName.setFont(Font.font("Abel", FontWeight.NORMAL, 15));
+					nutrientName.setMaxSize(120, 27);
+					nutrientName.setMinSize(120, 27);
 					TextField comparator = new TextField();
 					comparator.setPromptText("Comparator");
-					comparator.setMaxSize(100, 27);
-					comparator.setMinSize(100, 27);
+					comparator.setFont(Font.font("Abel", FontWeight.NORMAL, 15));
+					comparator.setMaxSize(120, 27);
+					comparator.setMinSize(120, 27);
 					TextField nutrientValue = new TextField();
 					nutrientValue.setPromptText("Nutrient value");
+					nutrientValue.setFont(Font.font("Abel", FontWeight.NORMAL, 15));
 					nutrientValue.setMaxSize(120, 27);
 					nutrientValue.setMinSize(120, 27);
 					Button confirmNutrient = new Button("Filter");
+					confirmNutrient.setFont(Font.font("Abel", FontWeight.NORMAL, 15));
 					confirmNutrient.setMinSize(100, 27);
 					nutrientBox.getChildren().addAll(nutrientName, comparator, nutrientValue, confirmNutrient);
-					Scene scene = new Scene(nutrientBox, 500, 50);
+					Scene scene = new Scene(nutrientBox, 520, 50);
 					Stage stage = new Stage();
 					stage.setTitle("Filter By Nutrient");
 					stage.setScene(scene);
@@ -143,44 +161,99 @@ public class Main extends Application {
 				}
 			}
 		});
-		// CheckBox nutrientFilter = new CheckBox("Nutrients");
-		// nutrientFilter.setIndeterminate(false);
+
 		filterHbox.getChildren().addAll(filterLbl, nameFilter, nutrientFilter);
 
 		// additional buttons for modifying food / meal
 		Button addToMealBtn = new Button("Add to Meal");
-		addToMealBtn.setPrefSize(200, 20);
+		addToMealBtn.setFont(Font.font("Abel", FontWeight.NORMAL, 15));
+		addToMealBtn.setMaxSize(300, 38);
+		addToMealBtn.setMinSize(300, 38);
 		Button addNewFoodBtn = new Button(" + Add New Food");
-		addNewFoodBtn.setPrefSize(200, 20);
-		Button removeFoodBtn = new Button(" - Remove Food");
-		removeFoodBtn.setPrefSize(200, 20);
-		Button loadFromFileBtn = new Button("Load from File");
-		loadFromFileBtn.setPrefSize(200, 20);
-		Button saveToFileBtn = new Button("Save to File");
-		saveToFileBtn.setPrefSize(200, 20);
-		vbox1.getChildren().addAll(foodLbl, filterHbox, listOfFoods, addToMealBtn, addNewFoodBtn, removeFoodBtn,
-				loadFromFileBtn, saveToFileBtn);
+		addNewFoodBtn.setFont(Font.font("Abel", FontWeight.NORMAL, 15));
+		addNewFoodBtn.setMaxSize(300, 38);
+		addNewFoodBtn.setMinSize(300, 38);
 
+		Button loadFromFileBtn = new Button("Load from File");
+		loadFromFileBtn.setFont(Font.font("Abel", FontWeight.NORMAL, 15));
+		loadFromFileBtn.setMaxSize(300, 38);
+		loadFromFileBtn.setMinSize(300, 38);
+		loadFromFileBtn.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				try {
+					FileChooser fileChooser = new FileChooser();
+					fileChooser.setTitle("Open food data file");
+					File selectedFile = fileChooser.showOpenDialog(primaryStage);
+					foodData.loadFoodItems(selectedFile.getPath());
+					foodItemList = (ArrayList<FoodItem>) foodData.getAllFoodItems();
+					foodNameList = new ArrayList<String>();
+					for (int i = 0; i < foodItemList.size(); i++)
+						foodNameList.add(foodItemList.get(i).getName());
+					foodNames.clear();
+					foodNames.setAll(FXCollections.observableArrayList(foodNameList));
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+		Button saveToFileBtn = new Button("Save to File");
+		saveToFileBtn.setFont(Font.font("Abel", FontWeight.NORMAL, 15));
+		saveToFileBtn.setMaxSize(300, 38);
+		saveToFileBtn.setMinSize(300, 38);
+		saveToFileBtn.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				try {
+					HBox saveFileHbox = new HBox();
+					saveFileHbox.setPadding(new Insets(15, 12, 15, 12));
+					saveFileHbox.setSpacing(10);
+					Label outputFileNameLbl = new Label("Name of output file");
+					outputFileNameLbl.setFont(Font.font("Abel", FontWeight.NORMAL, 15));
+					TextField inputField = new TextField();
+					Button confirmSaveBtn = new Button("Save");
+					confirmSaveBtn.setFont(Font.font("Abel", FontWeight.NORMAL, 13));
+					confirmSaveBtn.setMinSize(100, 27);
+					saveFileHbox.getChildren().addAll(outputFileNameLbl, inputField, confirmSaveBtn);
+					Scene scene = new Scene(saveFileHbox, 450, 50);
+					Stage stage = new Stage();
+					stage.setTitle("Save to file");
+					stage.setScene(scene);
+					stage.show();
+					confirmSaveBtn.setOnAction(ActionEvent -> {
+						stage.close();
+						foodData.saveFoodItems(inputField.getText());
+					});
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		vbox1.getChildren().addAll(foodLbl, filterHbox, listOfFoods, addToMealBtn, addNewFoodBtn, loadFromFileBtn,
+				saveToFileBtn);
+
+		// Line which divides the "All Foods" section from the "Current Meal" section
 		Line divider = new Line();
 		divider.setStartX(200.0f);
 		divider.setStartY(0.0f);
 		divider.setEndX(200.0f);
-		divider.setEndY(400.0f);
+		divider.setEndY(1000.0f);
 
+		// Vbox for the "Current Meal" section
 		VBox vbox2 = new VBox();
 		vbox2.setMaxWidth(600);
 		vbox2.setSpacing(12);
 
 		// text for diaplying current meal
-		Text currentMealLbl = new Text("                Current Meal");
-		currentMealLbl.setFont(Font.font("Verdana", FontWeight.BOLD, 30));
+		Text currentMealLbl = new Text("                             Current Meal");
+		currentMealLbl.setFont(Font.font("Abel", FontWeight.EXTRA_BOLD, 28));
 
 		HBox displayFoodNutritionHbox = new HBox();
 		displayFoodNutritionHbox.setSpacing(20);
 
 		VBox foodListAndRemoveVbox = new VBox();
 		foodListAndRemoveVbox.setSpacing(12);
-		foodListAndRemoveVbox.setMaxWidth(200);
+		foodListAndRemoveVbox.setMaxWidth(300);
+		foodListAndRemoveVbox.setMinWidth(300);
 
 		// food list for a meal. Hard coded at the moment. TODO: add more food / import
 		// from file
@@ -188,13 +261,15 @@ public class Main extends Application {
 				"Green beans", "Mashed potatoes", "Turkey");
 
 		ListView<String> listOfFoodsInMeal = new ListView<String>(foodInMealNames);
-		listOfFoodsInMeal.setMaxWidth(200);
-		listOfFoodsInMeal.setMinWidth(200);
-		listOfFoodsInMeal.setMaxHeight(300);
-		listOfFoodsInMeal.setMinHeight(300);
+		listOfFoodsInMeal.setMaxWidth(300);
+		listOfFoodsInMeal.setMinWidth(300);
+		listOfFoodsInMeal.setMaxHeight(391);
+		listOfFoodsInMeal.setMinHeight(391);
 
 		Button removeFoodFromMealBtn = new Button(" - Remove from Meal");
-		removeFoodFromMealBtn.setPrefSize(200, 20);
+		removeFoodFromMealBtn.setPrefSize(300, 38);
+		removeFoodFromMealBtn.setFont(Font.font("Abel", FontWeight.NORMAL, 15));
+
 		foodListAndRemoveVbox.getChildren().addAll(listOfFoodsInMeal, removeFoodFromMealBtn);
 
 		VBox nutritionVbox = new VBox();
@@ -203,7 +278,9 @@ public class Main extends Application {
 
 		HBox nutritionForFoodHbox = new HBox();
 		Label nutritionLbl = new Label("Nutrition for: ");
+		nutritionLbl.setFont(Font.font("Abel", FontWeight.NORMAL, 15));
 		Label foodNutritionLbl = new Label("Asparagus");
+		foodNutritionLbl.setFont(Font.font("Abel", FontWeight.NORMAL, 15));
 		nutritionForFoodHbox.getChildren().addAll(nutritionLbl, foodNutritionLbl);
 
 		ObservableList<String> nutritionInEachFood = FXCollections.observableArrayList("Calories: 20", "Fat: 0",
@@ -212,10 +289,11 @@ public class Main extends Application {
 		ListView<String> nutritionList = new ListView<String>(nutritionInEachFood);
 		nutritionList.setMaxWidth(320);
 		nutritionList.setMinWidth(320);
-		nutritionList.setMaxHeight(134);
-		nutritionList.setMinHeight(134);
+		nutritionList.setMaxHeight(128);
+		nutritionList.setMinHeight(128);
 
 		Label totalNutritionLbl = new Label("Total Nutrition");
+		totalNutritionLbl.setFont(Font.font("Abel", FontWeight.NORMAL, 15));
 
 		ObservableList<String> totalNutrition = FXCollections.observableArrayList("Calories: 2500", "Fat: 300",
 				"Carbohydrates: 200", "Fiber: 30", "Protein: 60");
@@ -223,8 +301,8 @@ public class Main extends Application {
 		ListView<String> totalNutritionList = new ListView<String>(totalNutrition);
 		totalNutritionList.setMaxWidth(320);
 		totalNutritionList.setMinWidth(320);
-		totalNutritionList.setMaxHeight(134);
-		totalNutritionList.setMinHeight(134);
+		totalNutritionList.setMaxHeight(128);
+		totalNutritionList.setMinHeight(128);
 		nutritionVbox.getChildren().addAll(nutritionForFoodHbox, nutritionList, totalNutritionLbl, totalNutritionList);
 		displayFoodNutritionHbox.getChildren().addAll(foodListAndRemoveVbox, nutritionVbox);
 		vbox2.getChildren().addAll(currentMealLbl, displayFoodNutritionHbox);
@@ -232,7 +310,7 @@ public class Main extends Application {
 		HBox hbox = new HBox();
 		hbox.setSpacing(20);
 		hbox.getChildren().addAll(vbox1, divider, vbox2);
-		primaryStage.setScene(new Scene(hbox, 800, 400));
+		primaryStage.setScene(new Scene(hbox, 1000, 500));
 		primaryStage.show();
 	}
 
